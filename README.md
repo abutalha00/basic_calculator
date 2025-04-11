836288d3ef61b3198341cd93439046cf6699d61b
abutalha00 <talhajb99@gmail.com>
2025-04-12 01:53:11 +0200

    all basic functionalities are added

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # basic_calculator
 This is a basic calculator writen in HTML, CSS and Javascript. You can do any changes you want to improve it. Thankyou
+
+##Functional Logic
+
+- Add number to input
+- Clear "prompt input" and then add operator
+- Add number and clear operaor
+- Click "equal" button to display "answer" & show full prompt "n1 Operator n2"
+
+- Clear button.click
+  - clear inputs and assign 0 to both inputs
+- Backspace button.click
+  - remove last "prompt digit"
+- Close button.click
+  - close calculator tab
+- Opetion button.click
+  - ... (pending)
--- js/app.js
+++ js/app.js
@@ -11,38 +11,75 @@ let operation = null;
 const operator = document.querySelectorAll(
   "#plus, #minus, #multiply, #division"
 );
+let number1 = 0,
+  number2 = 0;
+	result = 0;
+
+// initialize program
+initializePrograme();
+
+equal.addEventListener("click", calculateAnswer);
 
 for (let i = 0; i < number.length; i++) {
-	number[i].addEventListener("click", addNumberToPrompt);
+  number[i].addEventListener("click", addNumberToPrompt);
 }
 
-for (let i = 0; i < operator.length; i++) {	
-	operator[i].addEventListener("click", assignOperator);
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", assignOperator);
 }
 
-clear.addEventListener("click", () => prompt.value = "0");
-backspace.addEventListener("click", removeLastDigit)
+clear.addEventListener("click", initializePrograme);
+backspace.addEventListener("click", removeLastDigit);
 
 function addNumberToPrompt(event) {
-  let n = event.target.textContent;
-	if (operator !== null){
-		prompt.value = prompt.value != "0" ? prompt.value + n : n;
+  const n = event.target.textContent;
+
+	if (operation == null) {
+		if (prompt.value == 0) {
+			prompt.value = n;
+		} else {
+			prompt.value += n;
+		}
+		number1 = (prompt.value[0] == ".") ? 0 + prompt.value : prompt.value;
+	} else if (
+		prompt.value == "+" ||
+		prompt.value == "-" ||
+		prompt.value == "*" ||
+		prompt.value == "/"
+	) {
+		prompt.value = n;
+		number2 = prompt.value;
 	} else {
-		prompt.value = "0"
+		prompt.value += n;
+		number2 = (prompt.value[0] == ".") ? 0 + prompt.value : prompt.value;
 	}
-	
 }
 
 function assignOperator(event) {
   operation = event.target.textContent;
-	prompt.value = "0";
-	prompt.value = operation;
+  prompt.value = operation;
 }
 
 function removeLastDigit() {
-	let v = prompt.value.trim().split("");
-	v.pop();
-	v = v.join("")
-	prompt.value = v;
-	if (prompt.value == 0) prompt.value = "0";
+  let v = prompt.value.trim().split("");
+  v.pop();
+  v = v.join("");
+  prompt.value = v;
+  if (prompt.value == 0) prompt.value = "0";
+}
+
+function calculateAnswer() {
+	const calcString = `${number1} ${operation} ${number2}`;
+	// console.log(calcString)
+	// return;
+  result = eval(calcString);
+	answer.value = result;
+  prompt.value = calcString;
+  number1 = result;
+  operation = null;
+}
+
+function initializePrograme() {
+  prompt.value = "0";
+  answer.value = "";
 }
